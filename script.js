747da29c4b52bcf4b08bd793ef36dad55d355995
Izura <uchihaizura1219@gmail.com>
2021-05-10 20:31:52 +0300

    version 1

---
+++ script.js
@@ -0,0 +1,21 @@
+alert('Click on Dio');
+
+const div = document.querySelector('div');
+const bg = document.querySelector('.bg');
+const audioFile = document.querySelector('audio');
+const dio = document.querySelector('.dio');
+
+function clickOnDio() {
+	audioFile.play();
+	setTimeout( () => {
+		div.style.transform = "scale(15)"
+		bg.style.animation = "changeColor 1s linear 0s infinite"
+		dio.style.filter = 'invert(100%)'
+		setTimeout( () => {
+	 		div.style.transform = "scale(0)"
+	 		bg.style.animation = "none"
+	 		dio.style.filter = 'invert(0%)'
+	 	}
+	  	,10000);
+	},1400)
+}
